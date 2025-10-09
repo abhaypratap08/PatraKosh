9f66c3aa9adf951e4c5aa279dbab5da4db28ee58
Abhaypratap singh <pratapsinghabhay0208@gmail.com>
2025-10-09 22:36:52 +0530

    Add Windows batch script
    for easy application launch

---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+echo ****************************************
+echo    PatraKosh - File Storage Application
+echo ****************************************
+echo.
+echo Building and running the application...
+echo.
+
+mvn clean javafx:run
+
+pause
