ba3f447ce0cb817b8be299656132bbf32c2832f9
Abhaypratap singh <pratapsinghabhay0208@gmail.com>
2025-10-10 13:27:50 +0530

    Update setup-database.bat

--- setup-database.bat
+++ setup-database.bat
@@ -50,16 +50,107 @@ if not errorlevel 1 (
     )
 )
 
-:: MySQL not found
-echo [ERROR] MySQL not found!
+:: MySQL not found - offer to install
+echo [WARNING] MySQL not found!
 echo.
-echo Please install MySQL Server:
+echo Would you like to download and install MySQL automatically?
+echo This will download MySQL 8.0 Community Server (~400MB)
+echo.
+set /p install_mysql="Install MySQL automatically? (y/n): "
+
+if /i "%install_mysql%"=="y" goto install_mysql
+if /i "%install_mysql%"=="yes" goto install_mysql
+
+echo.
+echo Please install MySQL manually:
 echo Download from: https://dev.mysql.com/downloads/mysql/
 echo.
 echo After installation, run this script again.
 echo.
 pause
 exit /b 1
+
+:install_mysql
+echo.
+echo ==========================================
+echo    Installing MySQL Server 8.0
+echo ==========================================
+echo.
+echo This may take 10-15 minutes...
+echo Please be patient!
+echo.
+
+:: Create temp directory
+if not exist "%TEMP%\PatraKosh-MySQL" mkdir "%TEMP%\PatraKosh-MySQL"
+
+:: Download MySQL Installer
+echo [1/4] Downloading MySQL Installer (~2MB)...
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; try { Invoke-WebRequest -Uri 'https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-community-8.0.40.0.msi' -OutFile '%TEMP%\PatraKosh-MySQL\mysql-installer.msi' -UseBasicParsing } catch { Write-Host 'Download failed. Trying alternate link...'; Invoke-WebRequest -Uri 'https://cdn.mysql.com/Downloads/MySQLInstaller/mysql-installer-community-8.0.40.0.msi' -OutFile '%TEMP%\PatraKosh-MySQL\mysql-installer.msi' -UseBasicParsing }}"
+
+if not exist "%TEMP%\PatraKosh-MySQL\mysql-installer.msi" (
+    echo [ERROR] Failed to download MySQL installer
+    echo Please download manually from: https://dev.mysql.com/downloads/mysql/
+    pause
+    exit /b 1
+)
+
+echo [OK] MySQL Installer downloaded
+
+:: Install MySQL
+echo.
+echo [2/4] Installing MySQL Server...
+echo.
+echo IMPORTANT: During installation:
+echo 1. Choose "Server Only" installation type
+echo 2. Set a root password (remember it!)
+echo 3. Keep default settings for everything else
+echo.
+echo Press any key to start MySQL installation...
+pause >nul
+
+start /wait msiexec /i "%TEMP%\PatraKosh-MySQL\mysql-installer.msi" /qb
+
+echo.
+echo [3/4] Configuring MySQL...
+echo.
+
+:: Wait for MySQL service to be available
+timeout /t 5 >nul
+
+:: Try to start MySQL service
+net start MySQL80 >nul 2>&1
+if errorlevel 1 (
+    net start MySQL >nul 2>&1
+)
+
+:: Add MySQL to PATH
+echo [4/4] Adding MySQL to system PATH...
+
+if exist "C:\Program Files\MySQL\MySQL Server 8.0\bin" (
+    set "MYSQL_PATH=C:\Program Files\MySQL\MySQL Server 8.0\bin"
+    setx PATH "%PATH%;%MYSQL_PATH%" /M >nul 2>&1
+    set "PATH=%PATH%;%MYSQL_PATH%"
+    echo [OK] MySQL added to PATH
+) else (
+    echo [WARNING] MySQL installation path not found
+    echo You may need to add MySQL to PATH manually
+)
+
+:: Clean up
+rmdir /s /q "%TEMP%\PatraKosh-MySQL" >nul 2>&1
+
+echo.
+echo ==========================================
+echo    MySQL Installation Complete!
+echo ==========================================
+echo.
+echo MySQL Server 8.0 has been installed.
+echo.
+echo IMPORTANT: You need to restart this script
+echo to continue with database setup.
+echo.
+pause
+exit /b 0
 
 :mysql_found
 echo MySQL Location: %MYSQL_PATH%
